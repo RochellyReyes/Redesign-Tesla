75a2d2bd4c2d6ee2da278021036168ba926cfae8
RochellyReyes <72086482+RochellyReyes@users.noreply.github.com>
2021-02-24 09:25:50 -0500

    home page (#1)
    Co-authored-by: ashley-woodton <ashley.woodton@gmail.com>

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import Footer from "../Components/Footer"
+import Footer from "../Components/Footer";
+import ImageSlider from "../Components/Slider/Slider";
+import "../Styling/home.css"
+import { NavLink}  from 'react-router-dom'
 
 const Home = () => {
     return(
         <div>
-            <h1>Home Page</h1>
-            <p>dsfgagnfbytr fghrtfghtrf gtrefgsntr fgtrfgbntrfdg gfvbtrfsgdftrf refsdrfgbdht ggsgdfdfsgdbdgs dgfs dgdgvdgv tgvtgv dstgsvg sdvg sdrg vsdg vdg dv g vd gvd gfd gvdgv</p>
+            <ImageSlider></ImageSlider>
+            <section className="cards">
+                <div className="wrapper">
+                    <div className="box">
+                        <h2>Vehicles</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum eos omnis distinctio natus, ab rem, sit deleniti earum ut doloribus dolor. Commodi dolorum assumenda officiis voluptate facilis rem suscipit dolor.</p>
+                        <NavLink exact={true} to='/models'>See Our Cars</NavLink>
+                    </div>
+                    <div className="box">
+                        <h2>Solar</h2>
+                        <p>Use solar energy to power your home and reduce your dependence on the grid. Purchase solar at the lowest price of any national provider with Tesla's price match guarantee and take control of your monthly electricity bill.</p>
+                        <NavLink exact={true} to='/solar'>Go to Solar</NavLink>
+                    </div>
+                </div>
+            </section>
             <Footer/>
         </div>
     )
